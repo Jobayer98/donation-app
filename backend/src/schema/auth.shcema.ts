8a76e2781e91a65eeb6c8d1506362c740f8f4e8a
jobayer <jobayerrahman0220@gmail.com>
2026-02-09 18:33:21 +0600

    define login and register schema

---
+++ backend/src/schema/auth.shcema.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const RegisterSchema = z.object({
+  name: z.string().min(2),
+  email: z.email(),
+  role: z.optional(
+    z.enum(["DONOR", "FUND_RAISER", "ADMIN"]).default("FUND_RAISER"),
+  ),
+  password: z.string().min(6),
+});
+
+export const LoginSchema = z.object({
+  email: z.email(),
+  password: z.string().min(6),
+});
+
+export type registerDto = z.infer<typeof RegisterSchema>;
+export type loginDto = z.infer<typeof LoginSchema>;
