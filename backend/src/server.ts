5bbbd0d69f2949f16870ad91d87927533279f9fd
jobayer <jobayerrahman0220@gmail.com>
2026-02-09 17:08:21 +0600

    install express,zod and create prisma instance

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello world");
+import express from "express";
+import apiRoute from "./routes/routes";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use(apiRoute);
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
