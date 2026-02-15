20ad13a50d9f400f77088627c948d6b86bcb8d1b
jobayer <jobayerrahman0220@gmail.com>
2026-02-15 19:24:11 +0600

    implement async handler function and wrap all controller

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,82 +1,73 @@
 import { Request, Response } from "express";
 import { prisma } from "../lib/prisma";
-import { loginDto, registerDto } from "../schema/auth.shcema";
+import { loginDTO, registerDTO } from "../schema/auth.shcema";
 import { hashPassword, verifyPassword } from "../utils/auth";
 import { generateAccessToken } from "../utils/jwt";
+import asyncHandler from "../utils/asyncHandler";
 
-export async function register(req: Request, res: Response) {
-  try {
-    let data: registerDto = req.body;
-    const hashPass = await hashPassword(data.password);
+export const register = asyncHandler(async (req: Request, res: Response) => {
+  let data: registerDTO = req.body;
+  const hashPass = await hashPassword(data.password);
 
-    if (!hashPass) {
-      throw new Error("Error hashing password");
-    }
-
-    let user = await prisma.user.create({
-      data: {
-        name: data.name,
-        email: data.email,
-        password: hashPass,
-        role: data.role,
-      },
-    });
-
-    const token = generateAccessToken({ id: user.id, role: user.role });
-
-    res.status(201).json({
-      success: true,
-      message: "User register successfully",
-      data: {
-        id: user.id,
-        token,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error });
+  if (!hashPass) {
+    throw new Error("Error hashing password");
   }
-}
 
-export async function login(req: Request, res: Response) {
-  try {
-    const data: loginDto = req.body;
-    const user = await prisma.user.findUnique({
-      where: {
-        email: data.email,
-      },
-    });
+  let user = await prisma.user.create({
+    data: {
+      name: data.name,
+      email: data.email,
+      password: hashPass,
+      role: data.role,
+    },
+  });
 
-    if (!user) {
-      return res
-        .status(401)
-        .json({ success: false, message: "Invalid credentail." });
-    }
+  const token = generateAccessToken({ id: user.id, role: user.role });
 
-    const isMatch = await verifyPassword(data.password, user!.password);
+  res.status(201).json({
+    success: true,
+    message: "User register successfully",
+    data: {
+      id: user.id,
+      token,
+    },
+  });
+})
 
-    if (!isMatch) {
-      return res
-        .status(401)
-        .json({ success: false, message: "Invalid credentail." });
-    }
+export const login = asyncHandler(async (req: Request, res: Response) => {
+  const data: loginDTO = req.body;
+  const user = await prisma.user.findUnique({
+    where: {
+      email: data.email,
+    },
+  });
 
-    const token = generateAccessToken({ id: user!.id, role: user!.role });
-
-    const userWithoutPassword = {
-      id: user?.id,
-      name: user?.name,
-      email: user?.email,
-      token: token,
-    };
-
-    res.json({
-      success: true,
-      message: "Login successful.",
-      data: userWithoutPassword,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error });
+  if (!user) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Invalid credentail." });
   }
-}
+
+  const isMatch = await verifyPassword(data.password, user!.password);
+
+  if (!isMatch) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Invalid credentail." });
+  }
+
+  const token = generateAccessToken({ id: user!.id, role: user!.role });
+
+  const userWithoutPassword = {
+    id: user?.id,
+    name: user?.name,
+    email: user?.email,
+    token: token,
+  };
+
+  res.json({
+    success: true,
+    message: "Login successful.",
+    data: userWithoutPassword,
+  });
+});
--- backend/src/controllers/campaign.controller.ts
+++ backend/src/controllers/campaign.controller.ts
@@ -1,165 +1,122 @@
 import { Request, Response } from "express";
 import { prisma } from "../lib/prisma";
 import { campaignStatusDTO, createCampaignDTO } from "../schema/campaign.schema";
+import asyncHandler from "../utils/asyncHandler";
 
 
-export async function createCampaign(req: Request, res: Response) {
-  try {
-    let data: createCampaignDTO = req.body;
-    let campaign = await prisma.campaign.create({
-      data: {
-        title: data.title,
-        description: data.description,
-        goalAmount: data.goalAmount,
-        category: data.category,
-        startDate: data.startDate,
-        endDate: data.endDate,
-        raisedAmount: 0,
-        type: data.type,
-        fundraiserId: req.user!.id,
-      },
-    });
+export const createCampaign = asyncHandler(async (req: Request, res: Response) => {
+  let data: createCampaignDTO = req.body;
+  let campaign = await prisma.campaign.create({
+    data: {
+      title: data.title,
+      description: data.description,
+      goalAmount: data.goalAmount,
+      category: data.category,
+      startDate: data.startDate,
+      endDate: data.endDate,
+      raisedAmount: 0,
+      type: data.type,
+      fundraiserId: req.user!.id,
+    },
+  });
 
-    res.status(201).json({
-      success: true,
-      message: "Campaign created successfully",
-      data: campaign,
-    });
-  } catch (error) {
-    console.log(error);
-    res.json({ error: error });
-  }
-}
+  res.status(201).json({
+    success: true,
+    message: "Campaign created successfully",
+    data: campaign,
+  });
+});
 
-export async function findActiveCampaign(req: Request, res: Response) {
-  try {
-    let data = await prisma.campaign.findMany({
-      where: {
-        status: "ACTIVE",
-      },
-    });
+export const findActiveCampaign = asyncHandler(async (req: Request, res: Response) => {
+  let data = await prisma.campaign.findMany({
+    where: {
+      status: "ACTIVE",
+    },
+  });
 
-    res.json({
-      success: true,
-      message: "Campaigns retrieved successfully",
-      data,
-    });
-  } catch (error) {
-    res.json({
-      success: true,
-      message: "Failed to retrive campaign data",
-      error: error,
-    });
-  }
-}
+  res.json({
+    success: true,
+    message: "Campaigns retrieved successfully",
+    data,
+  });
+});
 
-export async function findAllCampaign(req: Request, res: Response) {
+export const findAllCampaign = asyncHandler(async (req: Request, res: Response) => {
   let { page = 1, limit = 10, status = undefined } = req.query;
-  try {
-    let data = await prisma.campaign.findMany({
-      skip: (Number(page) - 1) * Number(limit),
-      take: Number(limit),
-      where: {
-        status: status ? status as campaignStatusDTO : undefined,
-      },
-    });
+  let data = await prisma.campaign.findMany({
+    skip: (Number(page) - 1) * Number(limit),
+    take: Number(limit),
+    where: {
+      status: status ? status as campaignStatusDTO : undefined,
+    },
+  });
 
-    res.json({
-      success: true,
-      message: "Campaigns retrieved successfully",
-      data,
-    });
-  } catch (error) {
-    res.json({
-      success: true,
-      message: "Failed to retrive campaign data",
-      error: error,
-    });
-  }
-}
+  res.json({
+    success: true,
+    message: "Campaigns retrieved successfully",
+    data,
+  });
+});
 
-export async function findOneCampaign(req: Request, res: Response) {
-  try {
-    let data = await prisma.campaign.findUnique({
-      where: {
-        id: String(req.params.id),
-      },
-    });
+export const findOneCampaign = asyncHandler(async (req: Request, res: Response) => {
+  let data = await prisma.campaign.findUnique({
+    where: {
+      id: String(req.params.id),
+    },
+  });
 
-    res.json({
-      success: true,
-      message: "Campaign retrieved successfully",
-      data,
-    });
-  } catch (error) {
-    res.json({
-      success: true,
-      message: "Failed to retrive campaign data",
-      error: error,
-    });
-  }
-}
+  res.json({
+    success: true,
+    message: "Campaign retrieved successfully",
+    data,
+  });
+});
 
-export async function updateCampaign(req: Request, res: Response) {
-  try {
-    let data = await prisma.campaign.update({
-      where: {
-        id: String(req.params.id),
-      },
-      data: req.body,
-    });
+export const updateCampaign = asyncHandler(async (req: Request, res: Response) => {
+  let data = await prisma.campaign.update({
+    where: {
+      id: String(req.params.id),
+    },
+    data: req.body,
+  });
 
-    res.json({
-      success: true,
-      message: "Campaign updated successfully",
-      data,
-    });
-  } catch (error) {
-    console.log(error);
-    res.json({ error: error });
-  }
-}
+  res.json({
+    success: true,
+    message: "Campaign updated successfully",
+    data,
+  });
+});
 
-export async function publishCampaign(req: Request, res: Response) {
-  try {
-    let data = await prisma.campaign.update({
-      where: {
-        id: String(req.params.id),
-      },
-      data: {
-        status: "ACTIVE",
-      },
-    });
+export const publishCampaign = asyncHandler(async (req: Request, res: Response) => {
+  let data = await prisma.campaign.update({
+    where: {
+      id: String(req.params.id),
+    },
+    data: {
+      status: "ACTIVE",
+    },
+  });
 
-    res.json({
-      success: true,
-      message: "Campaign published successfully",
-      data,
-    });
-  } catch (error) {
-    console.log(error);
-    res.json({ error: error });
-  }
-}
+  res.json({
+    success: true,
+    message: "Campaign published successfully",
+    data,
+  });
+});
 
-export async function closeCampaign(req: Request, res: Response) {
-  try {
-    let data = await prisma.campaign.update({
-      where: {
-        id: String(req.params.id),
-      },
-      data: {
-        status: "PAUSED",
-      },
-    });
+export const closeCampaign = asyncHandler(async (req: Request, res: Response) => {
+  let data = await prisma.campaign.update({
+    where: {
+      id: String(req.params.id),
+    },
+    data: {
+      status: "PAUSED",
+    },
+  });
 
-    res.json({
-      success: true,
-      message: "Campaign closed successfully",
-      data,
-    });
-  } catch (error) {
-    console.log(error);
-    res.json({ error: error });
-  }
-}
+  res.json({
+    success: true,
+    message: "Campaign closed successfully",
+    data,
+  });
+});
